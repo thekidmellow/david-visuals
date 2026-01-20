61fc3b5ed6346ef447e051344153ad7f0ac95eeb
thekidmellow <thekidmellow@gmail.com>
2026-01-20 19:01:41 +0100

    feat: register orders in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'user', 'package', 'total', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('order_number', 'user__username', 'user__email', 'stripe_pid')
+    list_editable = ('status',)
+    readonly_fields = ('order_number', 'created_at', 'updated_at', 'stripe_pid')
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        ('Order Information', {
+            'fields': ('order_number', 'user', 'package', 'total', 'status')
+        }),
+        ('Payment Information', {
+            'fields': ('stripe_pid',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
