f73e7bf598e4bdcd16d3c0dbd98eca8edae41c39
thekidmellow <thekidmellow@gmail.com>
2026-01-20 20:37:21 +0100

    feat: register newsletter in admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Subscriber
 
-# Register your models here.
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at', 'is_active')
+    list_filter = ('is_active', 'subscribed_at')
+    search_fields = ('email',)
+    list_editable = ('is_active',)
+    readonly_fields = ('subscribed_at',)
+    ordering = ('-subscribed_at',)
