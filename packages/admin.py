eb81660ac5b3bd41773a5ef88e7e496741e842fc
thekidmellow <thekidmellow@gmail.com>
2026-01-20 15:21:14 +0100

    feat: register packages in admin

--- packages/admin.py
+++ packages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Package
 
-# Register your models here.
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'is_active', 'created_at')
+    list_filter = ('category', 'is_active', 'created_at')
+    search_fields = ('name', 'description')
+    list_editable = ('is_active', 'price')
+    ordering = ('category', 'price')
