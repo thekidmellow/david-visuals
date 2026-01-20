81e9e0214c5e74c9f8e132dc8e48360266772b2c
thekidmellow <thekidmellow@gmail.com>
2026-01-20 15:15:57 +0100

    feat: register home models in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Hero
 
-# Register your models here.
+
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('title', 'subtitle', 'description')
+    list_editable = ('is_active',)
+    ordering = ('-created_at',)
