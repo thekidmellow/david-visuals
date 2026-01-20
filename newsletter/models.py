9c5e52d57b9307d27befa76cf3474bafbfeb9697
thekidmellow <thekidmellow@gmail.com>
2026-01-20 20:34:27 +0100

    feat: create newsletter model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-subscribed_at']
+
+    def __str__(self):
+        return self.email
