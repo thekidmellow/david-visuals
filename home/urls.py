f00831aceaa3f2399468af186099814d03186025
thekidmellow <thekidmellow@gmail.com>
2026-01-21 10:19:56 +0100

    feat: add robots.txt for seo

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Hero
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,3 +9,17 @@ def index(request):
         'hero': hero,
     }
     return render(request, 'home/index.html', context)
+
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow: /admin/",
+        "Disallow: /accounts/",
+        "Disallow: /profile/",
+        "Disallow: /orders/",
+        "Allow: /",
+        "",
+        f"Sitemap: {request.scheme}://{request.get_host()}/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
