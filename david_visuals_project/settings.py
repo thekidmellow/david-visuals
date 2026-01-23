3970f00ab61179f1017110984cfdc88e85f3e0f3
thekidmellow <thekidmellow@gmail.com>
2026-01-23 10:56:57 +0100

    fix: Use basic StaticFilesStorage and let Heroku collect static

--- david_visuals_project/settings.py
+++ david_visuals_project/settings.py
@@ -139,7 +139,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
